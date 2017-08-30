256fdcc812774b4b6a4426cb07968c8842a29513
wangxilu <1531956991@qq.com>
2017-08-30 17:56:14 +0800

    redis配置修改

---
+++ supershopowner/src/main/java/com/xiaomai/supershopowner/common/RedisTest.java
@@ -0,0 +1,11 @@
+package com.xiaomai.supershopowner.common;
+
+
+
+	
+
+public class RedisTest {  
+	
+ 
+}  
+
---
+++ supershopowner/src/main/java/com/xiaomai/supershopowner/common/TopicMessageListener.java
@@ -0,0 +1,26 @@
+package com.xiaomai.supershopowner.common;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public class TopicMessageListener implements  MessageListener {  
+	private RedisTemplate redisTemplate;  
+    
+    public void setRedisTemplate(RedisTemplate redisTemplate) {  
+        this.redisTemplate = redisTemplate;  
+    } 
+	@Override  
+    public void onMessage(Message message, byte[] pattern) {  
+        byte[] body = message.getBody();//请使用valueSerializer  
+        byte[] channel = message.getChannel();  
+        String itemValue = (String)redisTemplate.getValueSerializer().deserialize(body);  
+        String topic = (String)redisTemplate.getStringSerializer().deserialize(channel);  
+        System.out.println("message" + topic); 
+        System.out.println("message" + itemValue); 
+    }  
+}  
