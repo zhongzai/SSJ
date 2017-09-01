4ea92b09958fddd6dc5a1ff5383244fdbcc65493
wangxilu <1531956991@qq.com>
2017-09-01 15:47:32 +0800

    添加店铺总销售数据接口

---
+++ supershopowner/src/main/java/com/xiaomai/supershopowner/entity/SalesTotalTranfer.java
@@ -0,0 +1,36 @@
+package com.xiaomai.supershopowner.entity;
+
+
+public class SalesTotalTranfer {
+    private Double totalSales;
+    private Integer totalNum;
+    private Double totalAvg;
+    private Integer totalGoods;
+	public Double getTotalSales() {
+		return totalSales;
+	}
+	public void setTotalSales(Double totalSales) {
+		this.totalSales = totalSales;
+	}
+	public Integer getTotalNum() {
+		return totalNum;
+	}
+	public void setTotalNum(Integer totalNum) {
+		this.totalNum = totalNum;
+	}
+	public Double getTotalAvg() {
+		return totalAvg;
+	}
+	public void setTotalAvg(Double totalAvg) {
+		this.totalAvg = totalAvg;
+	}
+	public Integer getTotalGoods() {
+		return totalGoods;
+	}
+	public void setTotalGoods(Integer totalGoods) {
+		this.totalGoods = totalGoods;
+	}
+    
+    
+    
+}
